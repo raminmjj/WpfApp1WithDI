84ff3cf8e96c950175fabb3eacc508fb3e143322
Ramin Alirezaee <ramin.alirezaee@hotmail.com>
2023-01-06 20:23:40 +0330

    fix: Making changes based on BionicCode's instructions
    https://stackoverflow.com/questions/72675530/wpf-dependency-injection-when-navigate-to-new-page#comment132361932_72676762

--- WpfApp1WithDI/App.xaml.cs
+++ WpfApp1WithDI/App.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfApp1WithDI
@@ -20,7 +15,18 @@ namespace WpfApp1WithDI
         public IServiceProvider ServiceProvider { get; set; }
         public IConfiguration Configuration { get; set; }
 
-        protected override void OnStartup(StartupEventArgs e)
+        private void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IPage1ViewModel, Page1ViewModel>();
+            serviceCollection.AddTransient(typeof(MainWindow));
+
+            serviceCollection.AddTransient<Home>();
+            serviceCollection.AddTransient<Page1>();
+            serviceCollection.AddSingleton<Func<Page1>>(serviceProvider => serviceProvider.GetService<Page1>);
+
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -34,16 +40,6 @@ namespace WpfApp1WithDI
             ServiceProvider = serviceCollection.BuildServiceProvider();
             var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
             mainWindow.Show();
-        }
-
-        private void ConfigureServices(ServiceCollection serviceCollection)
-        {
-            serviceCollection.AddTransient<IPage1ViewModel, Page1ViewModel>();
-            serviceCollection.AddTransient(typeof(MainWindow));
-
-            serviceCollection.AddTransient<Page1>();
-            serviceCollection.AddSingleton<Func<Page1>>(serviceProvider => serviceProvider.GetService<Page1>);
-
         }
     }
 }
